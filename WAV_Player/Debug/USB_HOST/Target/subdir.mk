96fe500b7f1c8ab012e5c1910cccc278e15a0688
RSI96 <radek96sikora@gmail.com>
2020-11-17 22:40:49 +0100

    WAV Player

---
+++ WAV_Player/Debug/USB_HOST/Target/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../USB_HOST/Target/usbh_conf.c 
+
+OBJS += \
+./USB_HOST/Target/usbh_conf.o 
+
+C_DEPS += \
+./USB_HOST/Target/usbh_conf.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+USB_HOST/Target/usbh_conf.o: ../USB_HOST/Target/usbh_conf.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../FATFS/Target -I../FATFS/App -I../USB_HOST/App -I../USB_HOST/Target -I../Middlewares/Third_Party/FatFs/src -I../Middlewares/ST/STM32_USB_Host_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Host_Library/Class/MSC/Inc -I../Audio -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"USB_HOST/Target/usbh_conf.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
